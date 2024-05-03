aacfdf14febd7b6d46060f2a4bb5f75599085b46
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2024-05-03 15:47:37 +0200

    idporten lets go

---
+++ src/idporten-client.js
@@ -0,0 +1,27 @@
+const { Issuer } = require('openid-client')
+const { IDPORTEN } = require('../config')
+
+let idPortenInstance = null
+
+/**
+ * 
+ * @returns {import('openid-client').BaseClient} idPortenClient
+ */
+const getIdPortenClient = async () => {
+  if (idPortenInstance) return idPortenInstance
+  
+  const issuer = await Issuer.discover(IDPORTEN.WELL_KNOWN_ENDPOINT)
+  const { Client } = issuer
+
+  idPortenInstance = new Client({
+      client_id: IDPORTEN.CLIENT_ID,
+      client_secret: IDPORTEN.ClIENT_SECRET,
+      redirect_uris: [IDPORTEN.ClIENT_REDIRECT_URI],
+      post_logout_redirect_uris: ['http://localhost:5173'], // HUSK å leggge dett eog i config etterhvert
+      response_types: ['code'],
+      token_endpoint_auth_method: 'client_secret_post'
+  })
+  return idPortenInstance
+}
+
+module.exports = { getIdPortenClient }
