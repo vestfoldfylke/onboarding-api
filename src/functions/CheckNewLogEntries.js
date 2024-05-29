cb513b0c529eed1fc7ace430ec085ae4985b7ae4
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2024-05-29 14:24:58 +0200

    idiot man strikes again

--- src/functions/CheckNewLogEntries.js
+++ src/functions/CheckNewLogEntries.js
@@ -5,12 +5,12 @@ const { logger } = require('@vtfk/logger')
 app.timer('CheckNewLogEntries', {
   schedule: '*/20 * * * *', // Hvert 20 minutt
   handler: async (myTimer, context) => {
-    logger('info', ['CheckNewLogEntries - new run', context])
+    logger('info', ['CheckNewLogEntries - new run'], context)
     try {
       const updateResult = await checkNewLogEntries(context)
-      logger('info', [`CheckNewLogEntries - finished running - result - ${updateResult}`, context])
+      logger('info', [`CheckNewLogEntries - finished running - result - ${updateResult}`], context)
     } catch (error) {
-      logger('error', ['CheckNewLogEntries - failed when updating user collection', error.response?.data || error.stack || error.toString()])
+      logger('error', ['CheckNewLogEntries - failed when updating user collection', error.response?.data || error.stack || error.toString()], context)
     }
   }
 })
--- src/functions/UpdateUserCollection.js
+++ src/functions/UpdateUserCollection.js
@@ -5,12 +5,12 @@ const { updateUsers } = require('../schedules/update-users')
 app.timer('UpdateUserCollection', {
   schedule: '0 0 2 ? * * *', // Kl 02:00 om natta
   handler: async (myTimer, context) => {
-    logger('info', ['UpdateUserCollection - new run', context])
+    logger('info', ['UpdateUserCollection - new run'], context)
     try {
       const updateResult = await updateUsers(context)
-      logger('info', [`UpdateUserCollection - finished running - result - ${updateResult}`, context])
+      logger('info', [`UpdateUserCollection - finished running - result - ${updateResult}`], context)
     } catch (error) {
-      logger('error', ['UpdateUserCollection - failed when updating user collection', error.response?.data || error.stack || error.toString()])
+      logger('error', ['UpdateUserCollection - failed when updating user collection', error.response?.data || error.stack || error.toString()], context)
     }
   }
 })
